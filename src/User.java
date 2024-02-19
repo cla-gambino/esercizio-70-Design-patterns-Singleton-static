4e9e1337e0415da5cd313af4c28ebc1a349c62c8
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-19 20:49:01 +0100

    update user

--- src/User.java
+++ src/User.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class User {
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    private static User instance;
+
+    private User (){}
+
+    public static User getInstance(){
+        if(instance == null){
+            instance = new User();
+            instance.name = "Claudio Gambino";
+            instance.age = 38;
+        }
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void printInformation(){
+        System.out.println("User name: " + name);
+        System.out.println("User age: " + age);
+    }
 }
